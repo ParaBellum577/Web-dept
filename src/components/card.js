af266a1147c79e7d64ea8ddde05c1e00679e9890
Roman <40899256+ParaBellum577@users.noreply.github.com>
2019-01-15 15:24:16 +0200

    align items JSX

--- src/components/card.js
+++ src/components/card.js
@@ -16,11 +16,40 @@ handleTitleChange = e => {
 render () {
   return (
 <div className='cell'>
-   <input type ="text" value={this.props.data.title} className ="inp" onChange={this.handleTitleChange} ></input>
-   <textarea type ="text" value={this.props.data.description} className ="inp" onChange={this.handleDescriptionChange}></textarea>
-   <button  disabled={!this.props.isRightEmpty} className="btn-rgt" onClick={()=> this.props.handleAddRight(this.props.rowIndex, this.props.cellIndex)}><i className="fa fa-plus" aria-hidden="true"></i></button>
-   <button disabled={!this.props.isBottomEmpty} className="btn-bot" onClick={()=>this.props.handleAddBottom(this.props.rowIndex, this.props.cellIndex)} ><i className="fa fa-plus" aria-hidden="true"></i></button>
-   <button style={this.props.rowIndex === 0 && this.props.cellIndex === 0 ? {display:'none'} : {}} className="btn-del" onClick={()=>this.props.handleDelete(this.props.rowIndex, this.props.cellIndex)}><i className="fa fa-times" aria-hidden="true"></i> </button>
+   <input 
+    type ="text" 
+    value={this.props.data.title} 
+    className ="inp" 
+    onChange={this.handleTitleChange} 
+    >
+    </input>
+   <textarea 
+    type ="text" 
+    value={this.props.data.description} 
+    className ="inp" 
+    onChange={this.handleDescriptionChange}
+    >
+    </textarea>
+   <button  
+    disabled={!this.props.isRightEmpty} 
+    className="btn-rgt" 
+    onClick={()=> this.props.handleAddRight(this.props.rowIndex, this.props.cellIndex)}
+    >
+      <i className="fa fa-plus" aria-hidden="true"></i></button>
+   <button 
+    disabled={!this.props.isBottomEmpty} 
+    className="btn-bot" 
+    onClick={()=>this.props.handleAddBottom(this.props.rowIndex, this.props.cellIndex)} 
+    >
+      <i className="fa fa-plus" aria-hidden="true"></i></button>
+   <button 
+    style={this.props.rowIndex === 0 && this.props.cellIndex === 0 
+           ? {display:'none'} 
+           : {}} 
+    className="btn-del"
+    onClick={()=>this.props.handleDelete(this.props.rowIndex, this.props.cellIndex)}
+    >
+      <i className="fa fa-times" aria-hidden="true"></i> </button>
  </div>
 
  );
